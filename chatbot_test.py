4dac457b14cb302ba83a02eb9654486060a1dc97
michael <kimh060612@naver.com>
2022-06-12 13:33:08 +0900

    .

--- chatbot_test.py
+++ chatbot_test.py
@@ -31,7 +31,9 @@ if __name__ == "__main__":
                 break
             a = ""
             while 1:
-                input_ids = torch.LongTensor(koGPT2_TOKENIZER.encode(Q_TKN + q + SENT + '0' + A_TKN + a)).unsqueeze(dim=0).to(device=device)
+                tokens = koGPT2_TOKENIZER.encode(Q_TKN + q + SENT + '0' + A_TKN + a)
+                print(tokens)
+                input_ids = torch.LongTensor(tokens).unsqueeze(dim=0).to(device=device)
                 pred = model(input_ids)
                 pred = pred.logits
                 gen = koGPT2_TOKENIZER.convert_ids_to_tokens(torch.argmax(pred, dim=-1).squeeze())[-1]
