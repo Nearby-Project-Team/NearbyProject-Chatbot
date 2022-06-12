3e6307626d9cc0e7c150945badd862a8779f0afa
michael <kimh060612@naver.com>
2022-06-12 18:44:48 +0900

    .

--- train.py
+++ train.py
@@ -38,7 +38,7 @@ train_dataloader = DataLoader(train_set, batch_size=16, num_workers=0, shuffle=T
 model.to(device)
 model.train()
 
-learning_rate = 5e-5
+learning_rate = 5e-3
 criterion = torch.nn.CrossEntropyLoss(reduction="none")
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=0.01)
 
