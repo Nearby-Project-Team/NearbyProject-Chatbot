d342fbb97710a5e2a5e75950cde8ab0f5d4eb986
michael <kimh060612@naver.com>
2022-06-12 13:16:28 +0900

    .

--- chatbot_test.py
+++ chatbot_test.py
@@ -21,18 +21,19 @@ koGPT2_TOKENIZER = PreTrainedTokenizerFast.from_pretrained("skt/kogpt2-base-v2",
             pad_token=PAD, mask_token=MASK) 
 model = GPT2LMHeadModel.from_pretrained('skt/kogpt2-base-v2')
 
-with torch.no_grad():
-    while 1:
-        q = input("user > ").strip()
-        if q == "quit":
-            break
-        a = ""
+if __name__ == "__main__":
+    with torch.no_grad():
         while 1:
-            input_ids = torch.LongTensor(koGPT2_TOKENIZER.encode(Q_TKN + q + SENT + '0' + A_TKN + a)).unsqueeze(dim=0)
-            pred = model(input_ids)
-            pred = pred.logits
-            gen = koGPT2_TOKENIZER.convert_ids_to_tokens(torch.argmax(pred, dim=-1).squeeze().numpy().tolist())[-1]
-            if gen == EOS:
+            q = input("user > ").strip()
+            if q == "quit":
                 break
-            a += gen.replace("▁", " ")
-        print("Chatbot > {}".format(a.strip()))
+            a = ""
+            while 1:
+                input_ids = torch.LongTensor(koGPT2_TOKENIZER.encode(Q_TKN + q + SENT + '0' + A_TKN + a)).unsqueeze(dim=0)
+                pred = model(input_ids)
+                pred = pred.logits
+                gen = koGPT2_TOKENIZER.convert_ids_to_tokens(torch.argmax(pred, dim=-1).squeeze().numpy().tolist())[-1]
+                if gen == EOS:
+                    break
+                a += gen.replace("▁", " ")
+            print("Chatbot > {}".format(a.strip()))
