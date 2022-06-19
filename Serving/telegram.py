2b25a6703dc729a7241ad9a1715bea533fe3d7f1
michael <kimh060612@naver.com>
2022-06-19 21:20:06 +0900

    .

--- Serving/telegram.py
+++ Serving/telegram.py
@@ -2,11 +2,12 @@ from flask import Flask, request, jsonify, make_response
 from flask_restful import marshal_with, Resource, reqparse, fields
 from flask_restful import Api
 import requests
+import os
  
 application = Flask(__name__)
 api = Api(application)
  
-botToken = "5326670284:AAFy4ZcYTyepp34cpUXU9y70E_dVixLHgEA" ##반드시 본인 챗봇의 토큰을 입력하시오
+botToken = os.environ['TOKEN'] ##반드시 본인 챗봇의 토큰을 입력하시오
  
 class Telebot(Resource):
     s = requests.session()
