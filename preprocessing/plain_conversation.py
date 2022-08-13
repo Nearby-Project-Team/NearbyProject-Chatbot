4313f4d6dd23699c0eecaea1c9a905d4f23aea1b
michael <kimh060612@naver.com>
2022-08-13 14:08:07 +0900

    .

--- preprocessing/plain_conversation.py
+++ preprocessing/plain_conversation.py
@@ -2,6 +2,7 @@ import pandas as pd
 import json
 import os 
 import glob
+from tqdm import tqdm
 
 DATA_PATH = "../ChatData/PlainConversation"
 SNS_LIST = os.listdir(DATA_PATH)
@@ -10,12 +11,31 @@ if __name__ == "__main__":
 
     data_dir_list = []
     for sns in SNS_LIST:
-        json_list = glob.glob(DATA_PATH + "/" + sns)
+        json_list = glob.glob(DATA_PATH + "/" + sns + "/*.json")
         data_dir_list.extend(json_list)
     
-    for d in data_dir_list:
+    dataCSV = {
+        "Q": [],
+        "A": [],
+        "label": []
+    }
+    
+    for d in tqdm(data_dir_list):
         data = open(d, "r")
         dataJson = json.load(data)
+        conversation = dataJson["info"][0]["annotations"]["lines"]
+        N = len(conversation)
+        for i in range(1, N):
+            pretxt = conversation[i - 1]["norm_text"] 
+            txt = conversation[i]["norm_text"]
+            dataCSV["Q"].append(pretxt)
+            dataCSV["A"].append(txt)
+            dataCSV["label"].append(0)
+    
+    df = pd.DataFrame(dataCSV)
+    df.to_csv("../ChatData/chatbot_QA_data_2.csv", index=False)
+    
+    
             
     
     
